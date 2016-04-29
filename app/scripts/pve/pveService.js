feb40884e0ca81f6879f22cd41cf5f33a5c1317d
iGerrebrands <gerrebrands.i@gmail.com>
2016-04-29 15:08:28 +0200

    Some small things

--- app/scripts/pve/pveService.js
+++ app/scripts/pve/pveService.js
@@ -7,37 +7,42 @@ angular
   ) {
     var actueelConceptList = [
       {
+        id: 0,
         title: 'PvE 8888',
         type: 'Beheer',
         date: '17 maart 2016',
         active: false
       },
       {
+        id: 1,
         title: 'PvE 5678',
         type: 'Beheer, Onderhoud',
         date: '17 maart 2016',
         active: false
       },
       {
+        id: 2,
         title: 'PvE 3',
         type: 'Onderhoud, Nieuwbouw',
         date: '18 februari 2016',
         active: false
       }
     ];
-
     var actueelVastgesteldList = [
       {
+        id: 4,
         title: 'PvE 8811',
         type: 'Onderhoud',
         date: '10 maart 2016'
       },
       {
+        id: 5,
         title: 'PvE 678',
         type: 'Beheer',
         date: '8 maart 2016'
       },
       {
+        id: 6,
         title: 'PvE 32',
         type: 'Onderhoud, Nieuwbouw',
         date: '5 februari 2016'
